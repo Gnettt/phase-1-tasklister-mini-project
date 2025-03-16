367c8293bad5d72ea1d413e9adb95be8e561db32
Jeannette Derek <jeannettederek1@gmail.com>
2025-03-16 21:24:53 +0300

    placed a remove button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+function addTask() {
+  let input = document.getElementById("new-task-description");
+  let taskText = input.value.trim();
+  if (taskText === "") return;
+  
+  let li = document.createElement("li");
+  li.innerHTML = `${taskText} <button class="delete" onclick="removeTask(this)">X</button>`;
+  document.getElementById("taskList").appendChild(li);
+  
+  input.value = "";
+}
+
+function removeTask(button) {
+  button.parentElement.remove();
+}
