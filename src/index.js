0e7a6d7a60d9400bcec11c75a24b3f642a795ee1
Jeannette Derek <jeannettederek1@gmail.com>
2025-03-24 19:56:17 +0300

    assign

--- src/index.js
+++ src/index.js
@@ -1,35 +1,44 @@
 document.addEventListener("DOMContentLoaded", () => {
- 
-    const form = document.getElementById("create-task-form");
-    const taskList = document.getElementById("tasks");
+    // your code here
   
+  const taskForm = document.querySelector("#create-task-form");
+  
+  const taskList = document.querySelector("#tasks");
+  const defaultTask = document.createElement ("li");
+  defaultTask.textContent = "Wash the dishes";
+  taskList.appendChild(defaultTask);
+  
+  taskForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+  
+    const taskDescription = document.querySelector("#new-task-description").value.trim();
     
-    form.addEventListener("submit", (event) => {
-      event.preventDefault(); 
+    if (taskDescription !=="") {
+      
+      const newTask = document.createElement("li");
+      newTask.textContent = taskDescription;
+      
+      taskList.appendChild(newTask);
+    
+      document.querySelector("#new-task-description").value = "";
+    }
+  });
   
-      const taskInput = document.getElementById("new-task-description")
-      const taskText = taskInput.value.trim();
+  const darkButton = document.createElement("button");
+  darkButton.textContent = "Dark Mode";
   
-      if (taskText === "") {
-        alert("Please fill out this field");
-        return;
+  darkButton.onclick = function () {
+      const bodyStyles = getComputedStyle(document.body);
+      const currentBgColor = bodyStyles.backgroundColor;
+  
+      if (currentBgColor === "rgb(0, 0, 0)") { // Check for black
+          document.body.style.backgroundColor = "white";
+          document.body.style.color = "black";
+      } else {
+          document.body.style.backgroundColor = "black";
+          document.body.style.color = "white";
       }
+  };
   
-      const listItem = document.createElement("li");
-      listItem.textContent = taskText;
-  
-      const deleteButton = document.createElement("button");
-      deleteButton.textContent = ("delete");
-      deleteButton.style.marginLeft = "10px";
-      deleteButton.addEventListener("click", () => {
-        listItem.remove();
-      });
-  
-      listItem.appendChild(deleteButton);
-  
-      taskList.appendChild(listItem);
-  
-      taskInput.value = "";
-  
-   });
+  document.querySelector("#main-content").insertBefore(darkButton, taskForm);
   });
